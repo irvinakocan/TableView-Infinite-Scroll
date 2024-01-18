06169f22017fb75931e4e7c8589f60687bd90bf5
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-01-18 15:34:49 +0100

    Initial Commit

---
+++ TableView Infinite Scroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView Infinite Scroll
+//
+//  Created by Macbook Air 2017 on 18. 1. 2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

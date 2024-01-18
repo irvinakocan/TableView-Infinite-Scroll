480660ce7389831c55ec082de9453433009526fc
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-01-18 17:16:27 +0100

    Implemented infinite scrolling/pagination

---
+++ TableView Infinite Scroll/APICaller.swift
@@ -0,0 +1,73 @@
+//
+//  APICaller.swift
+//  TableView Infinite Scroll
+//
+//  Created by Macbook Air 2017 on 18. 1. 2024..
+//
+
+import Foundation
+
+class APICaller {
+    
+    public static var isPaginating = false
+    
+    // mocking the data
+    
+    static func fetchData(pagination: Bool = false, completion: @escaping (Result<[String], Error>) -> Void) {
+        
+        if pagination {
+            isPaginating = true
+        }
+        
+        // we are creating a delay
+        DispatchQueue.global().asyncAfter(deadline: .now() + (pagination ? 3 : 2), execute: {
+            let originalData = [
+                "tomato",
+                "orange",
+                "potato",
+                "cucumber",
+                "banana",
+                "strawberry",
+                "tomato",
+                "orange",
+                "tomato",
+                "orange",
+                "potato",
+                "cucumber",
+                "banana",
+                "strawberry",
+                "potato",
+                "cucumber",
+                "banana",
+                "strawberry",
+                "tomato",
+                "orange",
+                "potato",
+                "cucumber",
+                "tomato",
+                "orange",
+                "potato",
+                "cucumber",
+                "banana",
+                "strawberry"
+            ]
+            let newData = [
+                "coffe",
+                "juice",
+                "iced coffe",
+                "sprite",
+                "comboucha",
+                "schweppes",
+                "tomato",
+                "orange",
+                "tomato",
+                "orange",
+                "potato"
+            ]
+            completion(.success(pagination ? newData : originalData))
+            if pagination {
+                isPaginating = false
+            }
+        })
+    }
+}
--- TableView Infinite Scroll/ViewController.swift
+++ TableView Infinite Scroll/ViewController.swift
@@ -8,12 +8,107 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var data = [String]()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setTableView()
     }
 
+    private func setTableView() {
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        APICaller.fetchData(completion: { [weak self] result in
+            switch result {
+            case .success(let data):
+                self?.data.append(contentsOf: data)
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(_):
+                break
+            }
+        })
+    }
+    
+    private func createSpinnerFooter() -> UIView {
+        let footerView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: view.frame.size.width,
+                                              height: 60))
+        
+        let spinner = UIActivityIndicatorView()
+        spinner.startAnimating()
+        footerView.addSubview(spinner)
+        spinner.center = footerView.center
+        
+        return footerView
+    }
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+}
+
+// UITableView is inherited from UIScrollView. ScrollView has a delegate who can tell us the position that user has scrolled down both horizontally and vertically, and from that we can figure out if we need to fetch more data.
+
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let position = scrollView.contentOffset.y
+        if position > (tableView.contentSize.height - 20 - scrollView.frame.size.height) {
+            
+            // fetch more data
+            
+            guard APICaller.isPaginating == false else {
+                // we are already fetching data
+                 return
+            }
+            
+            tableView.tableFooterView = createSpinnerFooter()
+            
+            APICaller.fetchData(pagination: true, completion: {[weak self] result in
+                DispatchQueue.main.async {
+                    self?.tableView.tableFooterView = nil
+                }
+                switch result {
+                case .success(let moreData):
+                    self?.data.append(contentsOf: moreData)
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
+                case .failure(_):
+                    break
+                }
+            })
+        }
+    }
 }
 
